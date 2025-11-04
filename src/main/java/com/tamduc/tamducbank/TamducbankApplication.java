4063c8553f2bc8553a6575af8943311022dd068b
Dao Duc Tam <ddt@gmail.com>
2025-11-04 13:00:58 +0700

    connected to database

---
+++ src/main/java/com/tamduc/tamducbank/TamducbankApplication.java
@@ -0,0 +1,13 @@
+package com.tamduc.tamducbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TamducbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TamducbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tamduc/tamducbank/TamducbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamduc.tamducbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TamducbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
